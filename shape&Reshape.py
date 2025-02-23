172e4edb662196178f684ab4892173eb43a18721
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2025-02-23 14:07:11 +0600

    shape&Reshape.py

--- shape&Reshape.py
+++ shape&Reshape.py
@@ -23,3 +23,8 @@ print(nparray4.shape)
 nparray5=nparray4.reshape(-1)
 print(nparray5)
 print(nparray5.shape)
+
+nparray6=np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
+print(nparray6.shape)
+nparray6=nparray6.reshape(4,3,2,1)
+print(nparray6)
